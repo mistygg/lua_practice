af9f536aae9294a39761bc04be8bc87a778e7c7c
xiaohaibao <xiaohaibao@pinduoduo.com>
2017-07-31 13:22:36 +0800

    sort.lua

--- sort.lua
+++ sort.lua
@@ -1,6 +1,19 @@
 sort = {}
 
+function sort.insert_sort(array)
+	for i=2,#array do
+		local key = array[i]
+		local x = i-1
+		for j=i-1,1,-1 do
+			if key<array[j] then
+				array[j+1] = array[j]
+			else
+				break
+			end
 
-
+		end
+		array[x] = key
+	end
+end
 
 return sort
